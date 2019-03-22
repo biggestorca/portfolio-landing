144cdf2e9ee30a42cdfb88c2e700422d3921fae8
biggestorca <imma.big.orca@gmail.com>
2019-03-22 17:28:38 +0200

    semantic

--- src/js/registerServiceWorker.js
+++ src/js/registerServiceWorker.js
@@ -1,14 +1,10 @@
 const registerServiceWorker = () => {
   if ('serviceWorker' in window.navigator) {
-    if (window.navigator.serviceWorker.controller) {
-      console.log('[PWA Builder] active service worker found, no need to register');
-    } else {
+    if (!window.navigator.serviceWorker.controller) {
       // Register the service worker
-      window.navigator.serviceWorker
-        .register('pwabuilder-sw.js', {
-          scope: './',
-        })
-        .then(reg => console.log('[PWA Builder] Service worker has been registered for scope: ', reg.scope));
+      window.navigator.serviceWorker.register('pwabuilder-sw.js', {
+        scope: './',
+      });
     }
   }
 };
