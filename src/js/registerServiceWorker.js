4ac20faa8860adb673be6c372af2298c31e53a99
biggestorca <imma.big.orca@gmail.com>
2019-03-22 19:50:36 +0200

    fix pwa

--- src/js/registerServiceWorker.js
+++ src/js/registerServiceWorker.js
@@ -1,10 +1,15 @@
 const registerServiceWorker = () => {
   if ('serviceWorker' in window.navigator) {
-    if (!window.navigator.serviceWorker.controller) {
+    if (window.navigator.serviceWorker.controller) {
+      console.log('[PWA Builder] active service worker found, no need to register');
+    } else {
       // Register the service worker
-      window.navigator.serviceWorker.register('pwabuilder-sw.js', {
-        scope: './',
-      });
+      window.navigator.serviceWorker
+        .register('pwabuilder-sw.js', {
+          scope: './',
+        })
+        .then(reg =>
+          console.log('[PWA Builder] Service worker has been registered for scope: ', reg.scope));
     }
   }
 };
--- src/pwa/pwabuilder-sw.js
+++ src/pwa/pwabuilder-sw.js
@@ -1,132 +1,65 @@
-// from https://www.pwabuilder.com/
+// This is the "Offline copy of pages" service worker
 
-// This is the service worker with the Advanced caching
+const CACHE = "pwabuilder-offline";
 
-const CACHE = 'pwabuilder-adv-cache';
-const precacheFiles = [
-  /* Add an array of files to precache for your app */
-];
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "index.html";
+const offlineFallbackPage = "index.html";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = 'ToDo-replace-this-name.html';
+// Install stage sets up the index page (home page) in the cache and opens a new cache
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Install Event processing");
 
-const networkFirstPaths = [
-  /* Add an array of regex of paths that should go network first */
-  // Example: /\/api\/.*/
-];
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Cached offline page during install");
 
-const avoidCachingPaths = [
-  /* Add an array of regex of paths that shouldn't be cached */
-  // Example: /\/api\/.*/
-];
-
-function pathComparer(requestUrl, pathRegEx) {
-  return requestUrl.match(new RegExp(pathRegEx));
-}
-
-function comparePaths(requestUrl, pathsArray) {
-  if (requestUrl) {
-    for (let index = 0; index < pathsArray.length; index++) {
-      const pathRegEx = pathsArray[index];
-      if (pathComparer(requestUrl, pathRegEx)) {
-        return true;
+      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
+        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
       }
-    }
-  }
 
-  return false;
-}
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(precacheFiles).then(() => {
-    if (offlineFallbackPage === 'ToDo-replace-this-name.html') {
-      return cache.add(new Response('TODO: Update the value of the offlineFallbackPage constant in the serviceworker.'));
-    }
-
-    return cache.add(offlineFallbackPage);
-  })));
-});
-
-// Allow sw to control of current page
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+      return cache.add(offlineFallbackPage);
+    })
+  );
 });
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
 
-  if (comparePaths(event.request.url, networkFirstPaths)) {
-    networkFirstFetch(event);
-  } else {
-    cacheFirstFetch(event);
-  }
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.log("[PWA Builder] add page to offline cache: " + response.url);
+
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+
+        return response;
+      })
+      .catch(function (error) {
+        console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
 });
-
-function cacheFirstFetch(event) {
-  event.respondWith(fromCache(event.request).then(
-    (response) => {
-      // The response was found in the cache so we responde with it and update the entry
-
-      // This is where we call the server to get the newest version of the
-      // file to use the next time we show view
-      event.waitUntil(fetch(event.request).then(response => updateCache(event.request, response)));
-
-      return response;
-    },
-    () =>
-    // The response was not found in the cache so we look for it on the server
-      fetch(event.request)
-        .then((response) => {
-          // If request was success, add or update it in the cache
-          event.waitUntil(updateCache(event.request, response.clone()));
-
-          return response;
-        })
-        .catch((error) => {
-          // The following validates that the request was for a navigation to a new document
-          if (event.request.destination !== 'document' || event.request.mode !== 'navigate') {
-            return;
-          }
-
-          // Use the precached offline page as fallback
-          return caches.open(CACHE).then((cache) => {
-            cache.match(offlineFallbackPage);
-          });
-        })
-    ,
-  ));
-}
-
-function networkFirstFetch(event) {
-  event.respondWith(fetch(event.request)
-    .then((response) => {
-      // If request was success, add or update it in the cache
-      event.waitUntil(updateCache(event.request, response.clone()));
-      return response;
-    })
-    .catch(error => fromCache(event.request)));
-}
 
 function fromCache(request) {
   // Check to see if you have it in the cache
   // Return response
   // If not in the cache, then return error page
-  return caches.open(CACHE).then(cache => cache.match(request).then((matching) => {
-    if (!matching || matching.status === 404) {
-      return Promise.reject('no-match');
-    }
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
 
-    return matching;
-  }));
+      return matching;
+    });
+  });
 }
 
 function updateCache(request, response) {
-  if (!comparePaths(request.url, avoidCachingPaths)) {
-    return caches.open(CACHE).then(cache => cache.put(request, response));
-  }
-
-  return Promise.resolve();
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
 }
