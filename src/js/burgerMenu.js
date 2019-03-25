7ee9bc0bfa9b0cb0ef5d44aa7f2e6232cb25549b
biggestorca <imma.big.orca@gmail.com>
2019-03-25 10:40:01 +0200

    add animation when nav close

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -16,13 +16,23 @@ const burgerMenu = () => {
           const $links = Array.prototype.slice.call($target.getElementsByTagName('a'), 0);
 
           // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
+          if (el.classList.contains('is-active')) {
+            el.classList.remove('is-active');
+            $target.classList.add('is-closed');
+            $target.classList.remove('is-active');
+            setTimeout(() => $target.classList.remove('is-closed'), 350);
+          } else {
+            el.classList.add('is-active');
+            $target.classList.remove('is-closed');
+            $target.classList.add('is-active');
+          }
+
+          // toggle($target);
 
           $links.forEach((link) => {
             link.addEventListener('click', () => {
               el.classList.remove('is-active');
-              $target.classList.remove('is-active');
+              setTimeout(() => $target.classList.remove('is-active'), 350);
             });
           });
 
